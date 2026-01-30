cf3f1db645338cdb5d46cc358f76beeb38cc4539
saikumar <saikumar.vundigam@gmail.com>
2026-01-30 14:31:40 +0530

    variables

--- Variable.sh
+++ Variable.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-PERSON1=Suresh
-PERSON2=Ramesh
+# PERSON1=Suresh
+# PERSON2=Ramesh
+
+# echo "$PERSON1:Hii Ramesh how are you...?"
+# echo "$PERSON2: Hii Suresh How are you...?"
+
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:Hii Ramesh how are you...?"
 echo "$PERSON2: Hii Suresh How are you...?"
